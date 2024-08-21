485d99bd1f7d0180d8f134030fd651baf38cf5a4
yuhneb1307 <88643091+yuhneb1307@users.noreply.github.com>
2024-08-21 14:41:44 +0700

    update css for patient route

--- routes/PatientsRoutes.js
+++ routes/PatientsRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const patientsController = require("../controllers/patientsController.js");
+router.use(express.static('public'));
 
 // Routes
 router.get("/", (req, res) => {
