92fbc5131df9711e77cc4ca3ff01944c8013611a
yuhneb1307 <88643091+yuhneb1307@users.noreply.github.com>
2024-09-09 13:09:31 +0700

    Create notification for user when enter wrong infor

--- public/js/main.js
+++ public/js/main.js
@@ -3,10 +3,14 @@ let role = document.getElementById("role");
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 let loginButton = document.getElementById("loginButton");
+let notification = document.getElementById("notification");
 
 loginButton.addEventListener("click", (e) => {
   e.preventDefault();
-  //   fetchData();
+  
+  // Hide notification by default
+  notification.classList.add("d-none");
+
   const formData = {
     email: email.value,
     password: password.value,
@@ -24,12 +28,11 @@ loginButton.addEventListener("click", (e) => {
   })
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error("Invalid email or password.");
       }
       return response.json();
     })
     .then((userData) => {
-      // Process the newly created user data
       if (userData.length == 1) {
         localStorage.setItem(role.value + "Data", JSON.stringify(userData));
         window.location.assign(
@@ -38,18 +41,9 @@ loginButton.addEventListener("click", (e) => {
       }
     })
     .catch((error) => {
+      // Show the error notification
+      notification.classList.remove("d-none");
+      notification.innerText = error.message;
       console.error("Error:", error);
     });
-});
-
-fetch("http://localhost:3000/patients/update-data", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  }
-}).then((response) => {
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  return response.json();
 });
